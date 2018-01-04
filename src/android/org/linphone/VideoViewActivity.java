e227fdf2ee7a0a5a60eac57caad3f5df8cacda81
Long Nguyen <nlhlong@protonmail.com>
2018-01-04 20:30:56 +0100

    add INFO sending

--- src/android/org/linphone/VideoViewActivity.java
+++ src/android/org/linphone/VideoViewActivity.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import org.linphone.core.LinphoneContent;
+import org.linphone.core.LinphoneContentImpl;
+import org.linphone.core.LinphoneCore;
+import org.linphone.core.LinphoneCoreListener;
+import org.linphone.core.LinphoneCoreListenerBase;
+import org.linphone.core.LinphoneInfoMessage;
+
 /**
  * Created by root on 18.12.17.
  */
@@ -12,6 +19,15 @@ public class VideoViewActivity extends LinphoneGenericActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        LinphoneCore lc = LinphoneManager.getLc();
+        LinphoneInfoMessage message = lc.createInfoMessage();
+        LinphoneContent content = new LinphoneContentImpl("text", null, null, null);
+        content.setStringData("start video");
+        message.setContent(content);
+        lc.getCurrentCall().sendInfoMessage(message);
+        LinphoneCoreListener lcListener = new LinphoneCoreListenerBase();
+
         setContentView(R.layout.video_view);
         VideoView videoView = findViewById(R.id.video_view);
         videoView.setVideoPath("http://192.168.1.100:8081/vod/sample_1280x720.mp4");
@@ -21,5 +37,6 @@ public class VideoViewActivity extends LinphoneGenericActivity {
         videoView.setMediaController(mediaController);
 
         videoView.start();
+
     }
 }
